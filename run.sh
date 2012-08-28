b242c8052a95befe8a58fe4f26461f6f12b38cff
Freddy Chua <freddycct@gmail.com>
2012-08-27 17:12:06 -0700

    updated for josh and ram

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 LC_ALL=C
 CP=bin:jar/mysql-connector-java-5.1.20-bin.jar:jar/JFlex.jar:jar/ark-1.0-SNAPSHOT.jar:jar/commons-cli-1.2.jar:jar/commons-math3-3.0.jar
 
-echo $1 
-echo $2 
-echo $3 
-echo $4
+echo arg1:$1 
+echo arg2:$2 
+echo arg3:$3 
+echo arg4:$4
 
-java -cp ${CP} visualize.Main $1 $2 $3 $4
- 
+cat /mnt/freddy/data/tweets/facebook_ipo_init.np | java -cp ${CP} visualize.Main $1 $2 $3 $4
+
